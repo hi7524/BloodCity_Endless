07e8d90efaa097184c5194042a8b27874791718a
Hi <105179934+hi7524@users.noreply.github.com>
2024-10-15 17:11:10 +0900

    플레이어 회전 공격 시스템 구현

--- Assets/Scripts/Player/Skills/HeavyMachineGun.cs
+++ Assets/Scripts/Player/Skills/HeavyMachineGun.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 // 플레이어 머신건 스킬
 public class HeavyMachineGun : MonoBehaviour, IPlayerSkill
 {
+    private Transform closetTarget; // 추적 대상
+
+    public Transform firePos;
     public Vector3 playerVec { get; set; } // 플레이어 위치
     public GameObject bulletPrf; // 총알 프리팹
     public float coolDown = 2;   // 스킬 쿨타임
 
     private float curCoolDown = 0;
 
+
+    private void Update()
+    {
+        DetectEnemy();
+        FollowEnemy();
+    }
 
     // 스킬 사용
     public void UseSkill()
@@ -18,8 +27,47 @@ public class HeavyMachineGun : MonoBehaviour, IPlayerSkill
 
         if(curCoolDown > coolDown)
         {
-            Instantiate(bulletPrf, playerVec, Quaternion.identity); // 총알 발사 (생성)
+            Instantiate(bulletPrf, firePos.position, Quaternion.identity); // 총알 발사 (생성)
             curCoolDown = 0;
+        }
+    }
+
+    // 적 감지 및 추적할 적 설정
+    private void DetectEnemy()
+    {
+        // 적 감지
+        MobAI[] enemies = FindObjectsOfType<MobAI>();
+        closetTarget = null;
+        float maxDis = Mathf.Infinity;
+
+        // 가장 가까운 적 설정
+        foreach (MobAI mob in enemies)
+        {
+            float targetDis = Vector2.Distance(transform.position, mob.transform.position);
+
+            if (targetDis < maxDis)
+            {
+                closetTarget = mob.transform;
+                maxDis = targetDis;
+            }
+        }
+    }
+
+    // 적 추적
+    private void FollowEnemy()
+    {
+        // 추적 대상이 존재하지 않을 경우
+        if (closetTarget == null)
+        {
+
+        }
+        // 추적 대상이 존재할 경우
+        else
+        {
+            Vector3 rotation = closetTarget.position - transform.position;
+            float rotationZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+            
+            transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.Euler(0, 0, rotationZ), 5 * Time.deltaTime);
         }
     }
 
