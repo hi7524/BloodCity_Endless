afabc661c7867e4e1eed07e1975054858ab97932
Hi <105179934+hi7524@users.noreply.github.com>
2024-10-20 17:52:35 +0900

    플레이어 스킬 구현
    - 드랍된 아이템 획득시 획득한 아이템 사용 가능
    - 기본 장착 무기 사용 가능

--- Assets/Scripts/Player/PlayerSkillPos.cs
+++ Assets/Scripts/Player/PlayerSkillPos.cs
@@ -1,16 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 // 플레이어 주위로 무기가 돌도록 함
 public class PlayerSkillPos : MonoBehaviour
 {
-    public GameObject examplePrf;
-    public float speed;
-    public float count;
+    [Header("스킬 설정값")]
+    public float speed = 15; // 무기 회전 속도
+    [Header("스킬")]
+    public GameObject defaultSkill; // 기본 무기 (샷건)
+    public List<GameObject> playerSkills = new List<GameObject>(); // 무기 목록
 
+    private float count;     // 무기 개수
+
+    
     private void Start()
     {
-        Batch();
+        count = 1;                      // 시작시 기본 무기 소지 시작 고정을 위한 개수 초기화
+        playerSkills.Add(defaultSkill); // 기본 무기 추가
+
+        Batch();                        // 초기 무기 배치
     }
+
     private void Update()
     {
         SkillRotate();
@@ -27,13 +37,47 @@ public class PlayerSkillPos : MonoBehaviour
     {
         for (int i = 0; i < count; i++)
         {
-            GameObject bullet = Instantiate(examplePrf); 
+            // 생성 및 설정
+            GameObject skill = Instantiate(playerSkills[i]);
+            skill.transform.parent = transform;
 
-            bullet.transform.parent = transform; 
+            // 위치값 초기화
+            skill.transform.localPosition = Vector3.zero; // 위치 초기화
+            skill.transform.localRotation = Quaternion.identity; // 회전 초기화
 
+            // 위치값 설정 및 배치
             Vector3 rotvec = Vector3.forward * 360 * i / count; // 위치 계산
-            bullet.transform.Rotate(rotvec);                    // 계산 위치에 배치
-            bullet.transform.Translate(bullet.transform.up * 5f, Space.World); // 플레이어로부터 거리 띄우기
+            skill.transform.Rotate(rotvec);                    // 계산 위치에 배치
+            skill.transform.Translate(skill.transform.up * 5f, Space.World); // 플레이어로부터 거리 띄우기
         }
+        
+    }
+
+    // 스킬 개수 추가 및 재배치
+    public void AddSkill()
+    {
+        // 기존 스킬 제거 (재배치를 위함)
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+
+        // 스킬 개수 추가
+        count++;
+
+        // 스킬 배치
+        Batch();
+    }
+
+    // Trrigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 스킬 충돌시 충돌 스킬 추가
+        if (collision.CompareTag("PlayerSkill"))
+        {
+            playerSkills.Add(collision.gameObject); // 충돌한 아이템 추가
+
+            AddSkill();
+        }   
     }
 }
