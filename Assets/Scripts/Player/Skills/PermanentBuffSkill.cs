13536d0da550923af1e4d49975a3633fc91c21bb
Hi <105179934+hi7524@users.noreply.github.com>
2024-10-21 18:06:45 +0900

    영구 강화 스킬 시스템 구현

---
+++ Assets/Scripts/Player/Skills/PermanentBuffSkill.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+// 영구 강화를 통해 얻는 스킬
+public class PermanentBuffSkill : MonoBehaviour
+{
+    // 플레임 밸트
+    // 파편 수류탄
+    // 얼음 수류탄
+
+    [Header("영구 강화 스킬")] 
+    public GameObject[] permanentSkills; // 영구 강화를 통해 얻을 수 있는 전체 스킬
+
+    private int skillCount = 0; // 현재 사용 스킬 개수
+
+    public void AddPermanentSkill(int skillID)
+    {
+        // 사용 스킬 최대 개수 2개 제한
+        if (skillCount < 2)
+        {
+            // 스킬 개수 추가
+            skillCount++;
+
+            // 스킬 활성화 (생성)
+            GameObject permanentSkill = Instantiate(permanentSkills[skillID]);
+            permanentSkill.transform.parent = transform;
+
+            // 위치값 초기화
+            permanentSkill.transform.localPosition = Vector3.zero; // 위치 초기화
+            permanentSkill.transform.localRotation = Quaternion.identity; // 회전 초기화
+        }
+        else
+        {
+            Debug.Log("이미 2개의 스킬을 사용하고 있습니다.");
+        }
+
+    }
+}
