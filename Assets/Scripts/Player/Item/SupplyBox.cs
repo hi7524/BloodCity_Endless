9b96fb960522de5a6dac4406a99276ba36cd39d7
Hi <105179934+hi7524@users.noreply.github.com>
2024-11-13 10:48:42 +0900

    애니메이션 적용
    - 무기 강화 보급 상자 열리는 애니메이션 재생 후 강화창 활성화
    - 보급 상자 오픈 효과음 적용
    - 플레이어 사망 후 결과창 활성화
    - 플레이어 추가 (우주해병, 바즈)

--- Assets/Scripts/Player/Item/SupplyBox.cs
+++ Assets/Scripts/Player/Item/SupplyBox.cs
@@ -3,18 +3,28 @@ using UnityEngine;
 // 보급 상자
 public class SupplyBox : MonoBehaviour, IItem
 {
-    private Animator animator;
+    public AudioClip openBoxSound;
 
+    private AudioSource audioSource;
+    private Animator animator;
 
     private void Awake()
     {
         // 컴포넌트 초기화
+        audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
+
     }
 
     public void Use(GameObject target)
     {
         animator.SetTrigger("BoxOpen");
+        audioSource.clip = openBoxSound;
+        audioSource.Play();
+    }
+
+    public void OpenWindow()
+    {
         UIManager.Instance.WeaponLevelUp();
     }
 }
