b6f23ab594518db8af9e4a99f4e1418dc879d8b7
langgusy <126739581+langgusy@users.noreply.github.com>
2024-11-14 11:48:13 +0900

    스테이지 작업
    늦어져서 죄송합니다람쥐

--- Assets/Scripts/LightEffect.cs
+++ Assets/Scripts/LightEffect.cs
@@ -1,16 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+// 2D 빛 깜빡임 이펙트
 public class LightEffect : MonoBehaviour
 {
     private Light2D light;
+    private float originIntensity; // 기존 조명 밝기
+
 
     private void Start()
     {
-        light.intensity = 0;
+        light = GetComponent<Light2D>();
+        originIntensity = light.intensity;
+
+        StartCoroutine(LightOff());
     }
 
+    IEnumerator LightOff()
+    {
+        while (true)
+        {
+            float lightDuration = Random.Range(0.5f, 1.5f);
 
+            light.intensity = originIntensity;
+            yield return new WaitForSeconds(lightDuration);
+            light.intensity = 0;
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
 }
