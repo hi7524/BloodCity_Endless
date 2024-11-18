2871b75ea539a83dfdb881d3e7ad950ec6239b54
gray_water <gray_water@kakao.com>
2024-11-18 22:14:17 +0900

    몬스터 애니메이션 적용 6, 인게임 조정 1
    [애니메이션]
    - 짐승 1종
    
    [조정]
    - 버스 크기 조정
    - 보스 유형 몬스터 물리 판정 수정
    - 시연용 밸런스 적용

--- Assets/Scripts/Monster/MobObjs/9_Beast/Skill/Mob_09_Beast_Rush.cs
+++ Assets/Scripts/Monster/MobObjs/9_Beast/Skill/Mob_09_Beast_Rush.cs
@@ -35,6 +35,7 @@ public class Mob_09_Beast_Rush : MonoBehaviour, IMobSkill
 
             var dis = Vector2.Distance(transform.position, AI.Target.transform.position); // 거리 차 계산
             var dir = (AI.Target.transform.position - transform.position).normalized; // 방향 계산
+            AI.render.flipX = (!AI.isReverseSprite ? dir.x > 0 : dir.x < 0); // 방향 전환
 
             AI.Mob.GetComponent<Rigidbody2D>().AddForce(dir * 17.5f, ForceMode2D.Impulse);
 
