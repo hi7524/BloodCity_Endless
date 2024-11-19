27ba239cb9b5b96376345dd7068e64c35909f89e
soen <76184818+soen18@users.noreply.github.com>
2024-11-19 23:43:15 +0900

    빌드버전 2

--- Assets/Scripts/UI/InGame/GameResult.cs
+++ Assets/Scripts/UI/InGame/GameResult.cs
@@ -28,10 +28,10 @@ public class GameResult : MonoBehaviour
     // 결과창에 떠야 하는 것
     public void Result()
     {
-        result.text = $"생존 시간 :                {TimeManager.Instance.nowMin}:{TimeText.Instance.sec}" +
-            $"도달한 레벨 :             {PlayerLevel.Instance.playerLevel} Lv" +
-            $"처치한 적 :         {KillText.Instance.kill} 마리" +
-            $"획득한 코인 :           {GameManager.Instance.coin} G";
+        result.text = $"생존 시간 :                {TimeManager.Instance.nowMin}:{TimeText.Instance.sec.ToString("f0")}\n" +
+            $"도달한 레벨 :             {PlayerLevel.Instance.playerLevel} Lv\n" +
+            $"처치한 적 :         {KillText.Instance.kill} 마리\n" +
+            $"획득한 코인 :           {GameManager.Instance.coin} G\n";
 
         BackBtn.SetActive(true);
     }
