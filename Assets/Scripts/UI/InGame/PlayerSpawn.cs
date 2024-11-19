c0b6d26c56505fa0c14eb5b44de504924b71be70
soen <76184818+soen18@users.noreply.github.com>
2024-11-19 16:37:19 +0900

    수정3

---
+++ Assets/Scripts/UI/InGame/PlayerSpawn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public static PlayerSpawn Instance;
+
+    public GameObject spaceMarine;
+    public GameObject beeper;
+    public GameObject baz;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void Spawn()
+    {
+        if (DataManager.Instance.player.currentIndex == 0) { spaceMarine.SetActive(true); }
+        if (DataManager.Instance.player.currentIndex == 1) { beeper.SetActive(true); }
+        if (DataManager.Instance.player.currentIndex == 2) { baz.SetActive(true); }
+    }
+}
