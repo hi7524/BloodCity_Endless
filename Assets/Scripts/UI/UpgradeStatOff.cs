a528dac31c2cf5d92dd24b6efcc73bc889ed73fa
soen <76184818+soen18@users.noreply.github.com>
2024-12-08 19:11:24 +0900

    버그 픽스 5
    - 총기 애니메이션 프레임 변경

--- Assets/Scripts/UI/UpgradeStatOff.cs
+++ Assets/Scripts/UI/UpgradeStatOff.cs
@@ -6,7 +6,6 @@ public class UpgradeStatOff : MonoBehaviour
 {
     public void WaitForAnimationEnd()
     {
-        Debug.Log("왜안껴ㅓ져");
         UIManager.Instance.levelUpWindow.SetActive(false);
         Time.timeScale = 1.0f;
     }
